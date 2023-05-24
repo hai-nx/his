bd8bb895c1df73a114d6726cf9e931381f5af6c3
duynv <duynguyen.19x@gmail.com>
2023-05-24 17:01:05 +0700

    Add Default.vue

---
+++ src/layouts/Default.vue
@@ -0,0 +1,18 @@
+<template>
+    <slot></slot>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'DefaultLayout',
+    setup() {
+
+
+        return {
+
+        }
+    }
+});
+</script>
