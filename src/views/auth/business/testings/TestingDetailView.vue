9b4e0258a7c3e70dbd62c156f3eb5c121f6875fa
duynguyen19x <105050939+duynguyen19x@users.noreply.github.com>
2024-01-08 22:00:34 +0700

    .

--- src/views/auth/business/testings/TestingDetailView.vue
+++ src/views/auth/business/testings/TestingDetailView.vue
@@ -4,132 +4,168 @@
         :visible="popupVisible"
         :width="1280"
         :height="650"
-        :hide-on-outside-click="true"
+        :hide-on-outside-click="false"
         :show-close-button="true"
+        :drag-enabled="true"
         title="Nhập kết quả xét nghiệm"
     >
-        <div class="content">
-            <DxLabel :visible="true" :text="'ádasdasdadsadas'"
-                >ádadsasd</DxLabel
-            >
-            <label class="grid-column-1"> Mã bệnh nhân: </label>
-            <label class="grid-column-2 bold">
-                {{ masterSource?.patientCode }}
-            </label>
-            <label class="grid-column-3"> Mã điều trị: </label>
-            <label class="grid-column-4 bold">
-                {{ masterSource?.patientCode }}
-            </label>
-            <label class="grid-column-5"> Tên bệnh nhân: </label>
-            <label class="grid-column-6 bold">
-                {{ masterSource?.patientName }}
-            </label>
+        <div class="grid-root row-1">
+            <div class="content">
+                <label class="grid-column-1"> Mã bệnh nhân: </label>
+                <label class="grid-column-2 bold">
+                    {{ masterSource?.patientCode }}
+                </label>
+                <label class="grid-column-3"> Mã điều trị: </label>
+                <label class="grid-column-4 bold">
+                    {{ masterSource?.patientCode }}
+                </label>
+                <label class="grid-column-5"> Tên bệnh nhân: </label>
+                <label class="grid-column-6 bold">
+                    {{ masterSource?.patientName }}
+                </label>
 
-            <label class="grid-column-1"> Số phiếu: </label>
-            <label class="grid-column-2 bold">
-                {{ masterSource?.serviceRequestCode }}
-            </label>
-            <label class="grid-column-3"> Barcode: </label>
-            <label class="grid-column-4 bold">
-                {{ masterSource?.barcode }}
-            </label>
-            <label class="grid-column-5"> Thời gian chỉ định: </label>
-            <label class="grid-column-6">
-                {{ masterSource?.serviceRequestDate }}
-            </label>
+                <label class="grid-column-1"> Số phiếu: </label>
+                <label class="grid-column-2 bold">
+                    {{ masterSource?.serviceRequestCode }}
+                </label>
+                <label class="grid-column-3"> Barcode: </label>
+                <label class="grid-column-4 bold">
+                    {{ masterSource?.barcode }}
+                </label>
+                <label class="grid-column-5"> Thời gian chỉ định: </label>
+                <label class="grid-column-6">
+                    {{ masterSource?.serviceRequestDate }}
+                </label>
 
-            <label class="grid-column-1"> Bác sỹ chỉ định: </label>
-            <label class="grid-column-2">
-                {{ masterSource?.userName }}
-            </label>
-            <label class="grid-column-3"> Khoa chỉ định: </label>
-            <label class="grid-column-4">
-                {{ masterSource?.departmentName }}
-            </label>
-            <label class="grid-column-5"> Thời gian trả KQ: </label>
-            <label class="grid-column-6">
-                {{ masterSource?.serviceRequestDate }}
-            </label>
+                <label class="grid-column-1"> Bác sỹ chỉ định: </label>
+                <label class="grid-column-2">
+                    {{ masterSource?.userName }}
+                </label>
+                <label class="grid-column-3"> Khoa chỉ định: </label>
+                <label class="grid-column-4">
+                    {{ masterSource?.departmentName }}
+                </label>
+                <label class="grid-column-5"> Thời gian trả KQ: </label>
+                <label class="grid-column-6">
+                    {{ masterSource?.serviceRequestDate }}
+                </label>
 
-            <label class="grid-column-1"> Chẩn đoán: </label>
-            <label class="grid-columnspan-2-7">
-                {{ masterSource?.icdName }}
-            </label>
-            <br />
-            <DxDataGrid
-                class="grid-columnspan-1-7 custom-data-grid"
-                :allow-column-reordering="true"
-                :data-source="detailSources"
-                :show-borders="true"
-                :hover-state-enabled="true"
-                key-expr="id"
-            >
-                <DxEditing
-                    :allow-updating="true"
-                    :allow-adding="false"
-                    :allow-deleting="false"
-                    mode="cell"
-                />
-                <!-- 'row' | 'batch' | 'cell' | 'form' | 'popup' -->
+                <label class="grid-column-1"> Chẩn đoán: </label>
+                <label class="grid-columnspan-2-7">
+                    {{ masterSource?.icdName }}
+                </label>
+            </div>
 
-                <DxColumn
-                    caption="Tên dịch vụ"
-                    data-field="serviceName"
-                    :visible="true"
-                    data-type="string"
-                    :group-index="0"
-                    :allow-editing="false"
-                />
-                <DxColumn
-                    caption="Mã xét nghiệm"
-                    data-field="serviceResultIndiceCode"
-                    :visible="true"
-                    data-type="string"
-                    :allow-editing="false"
-                />
-                <DxColumn
-                    caption="Tên xét nghiệm"
-                    data-field="serviceResultIndiceName"
-                    :visible="true"
-                    data-type="string"
-                    :allow-editing="false"
-                />
-                <DxColumn
-                    caption="Kết quả"
-                    data-field="result"
-                    :visible="true"
-                    :allow-editing="true"
-                    data-type="number"
-                    format="#,##0.##"
-                    edit-cell-template="result-edit-cell-template"
-                />
-                <DxColumn
-                    caption="Bình thường"
-                    data-field="normalRange"
-                    :visible="true"
-                    data-type="string"
-                    :allow-editing="false"
-                />
-                <DxColumn
-                    caption="Máy xét nghiệm"
-                    data-field="testingMachine"
-                    :visible="true"
-                    data-type="string"
-                    :allow-editing="false"
-                />
-                <template #result-edit-cell-template="{ data: cell }">
-                    <DxTextBox
-                        v-if="!cell.data.isNumber"
-                        v-model="cell.data.result"
+            <div class="row-2">
+                <DxDataGrid
+                    :allow-column-reordering="true"
+                    :data-source="detailSources"
+                    :show-borders="true"
+                    :hover-state-enabled="true"
+                    key-expr="id"
+                >
+                    <DxEditing
+                        :allow-updating="true"
+                        :allow-adding="false"
+                        :allow-deleting="false"
+                        mode="cell"
                     />
-                    <DxNumberBox
-                        v-else
+                    <!-- 'row' | 'batch' | 'cell' | 'form' | 'popup' -->
+
+                    <DxColumn
+                        caption="Tên dịch vụ"
+                        data-field="serviceName"
+                        :visible="true"
+                        data-type="string"
+                        :group-index="0"
+                        :allow-editing="false"
+                    />
+                    <DxColumn
+                        caption="Mã xét nghiệm"
+                        data-field="serviceResultIndiceCode"
+                        :visible="true"
+                        data-type="string"
+                        :allow-editing="false"
+                    />
+                    <DxColumn
+                        caption="Tên xét nghiệm"
+                        data-field="serviceResultIndiceName"
+                        :visible="true"
+                        data-type="string"
+                        :allow-editing="false"
+                    />
+                    <DxColumn
+                        caption="Kết quả"
+                        data-field="result"
+                        :visible="true"
+                        :allow-editing="true"
+                        data-type="number"
+                        alignment="left"
                         format="#,##0.##"
-                        v-model="cell.data.result"
-                        :twoWayBindingEnabled="false"
+                        edit-cell-template="result-edit-cell-template"
                     />
-                </template>
-            </DxDataGrid>
+                    <DxColumn
+                        caption="Bình thường"
+                        data-field="normalRange"
+                        :visible="true"
+                        data-type="string"
+                        :allow-editing="false"
+                    />
+                    <DxColumn
+                        caption="Máy xét nghiệm"
+                        data-field="testingMachine"
+                        :visible="true"
+                        data-type="string"
+                        :allow-editing="false"
+                    />
+                    <template #result-edit-cell-template="{ data: cell }">
+                        <DxTextBox
+                            v-if="!cell.data.isNumber"
+                            v-model="cell.data.result"
+                        />
+                        <DxNumberBox
+                            v-else
+                            format="#,##0.##"
+                            v-model="cell.data.result"
+                            :twoWayBindingEnabled="false"
+                        />
+                    </template>
+                </DxDataGrid>
+            </div>
+
+            <div class="row-3 footer">
+                <div class="footer-Col-1"></div>
+                <div class="footer-Col-2">
+                    <DxButton
+                        class="btn-DxButton"
+                        text="Tiếp nhận BP"
+                        :width="120"
+                        type="default"
+                        styling-mode="outlined"
+                    />
+                    <DxButton
+                        class="btn-DxButton"
+                        text="Hủy tiếp nhận"
+                        :width="120"
+                        type="danger"
+                        styling-mode="outlined"
+                    />
+                    <DxButton
+                        class="btn-DxButton"
+                        text="Trả kết quả"
+                        :width="120"
+                        type="default"
+                        styling-mode="outlined"
+                    />
+                    <DxButton
+                        class="btn-DxButton"
+                        text="Hủy trả KQ"
+                        :width="120"
+                        type="danger"
+                        styling-mode="outlined"
+                    />
+                </div>
+            </div>
         </div>
     </DxPopup>
 </template>
@@ -147,6 +183,7 @@ import {
 import { DxLabel } from "devextreme-vue/chart";
 import { DxTextBox } from "devextreme-vue/text-box";
 import DxNumberBox from "devextreme-vue/number-box";
+import DxButton from "devextreme-vue/button";
 
 export default defineComponent({
     name: "TestingDetailView",
@@ -198,19 +235,32 @@ export default defineComponent({
         // DxLookup,
         DxNumberBox,
         DxTextBox,
+        DxButton,
 
-        DxLabel,
+        // DxLabel,
     },
 });
 </script>
 
 <style scoped>
+.grid-root {
+    display: grid;
+    height: 100%;
+    grid-template-rows: auto 1fr 30px;
+    grid-row-gap: 15px;
+}
+
 .content {
     display: grid;
     grid-template-columns: repeat(3, auto minmax(50px, 1fr));
     grid-row-gap: 5px;
     grid-column-gap: 10px;
     align-items: center;
+}
+
+.footer {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
 }
 
 .grid-column-1 {
@@ -244,10 +294,22 @@ export default defineComponent({
     grid-column: 1/7;
 }
 
-.custom-data-grid
-    .dx-datagrid-headers
-    .dx-datagrid-content
-    .dx-datagrid-caption {
-    text-align: left;
+.footer-Col-2 {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+}
+
+.footer-Col-2 {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-end;
+}
+
+.btn-DxButton {
+    width: 120px;
+    margin: 0px 0px 0px 10px;
 }
 </style>
