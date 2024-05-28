3cdb45d3b5b178848f34b07c5955b6dd4e84dc90
hai-nx <72272795+hai-nx@users.noreply.github.com>
2024-05-28 16:12:27 +0700

    update

--- src/components/index.ts
+++ src/components/index.ts
@@ -11,11 +11,7 @@ export interface XItemType {
 import { defineAsyncComponent, App } from 'vue'
 
 export function defineAsyncComponents(app: App<Element>) {
-    app.component("XDateRangePicker", defineAsyncComponent(() => import('@/components/XDateRangePicker.vue')));
     app.component("XLayout", defineAsyncComponent(() => import('@/components/XLayout.vue')));
     app.component("XNav", defineAsyncComponent(() => import('@/components/XNav.vue')));
     app.component("XNavItem", defineAsyncComponent(() => import('@/components/XNavItem.vue')));
-    app.component("XPagination", defineAsyncComponent(() => import('@/components/XPagination.vue')));
-
-    app.component("XTable", defineAsyncComponent(() => import('@/components/XTable.vue')));
 }
