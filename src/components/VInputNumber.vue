6499dee1f8136fede2332f11219b00e9872dd037
hai-nx <72272795+hai-nx@users.noreply.github.com>
2024-07-05 23:57:39 +0700

    update

--- src/components/VInputNumber.vue
+++ src/components/VInputNumber.vue
@@ -8,15 +8,15 @@ import DxNumberBox from 'devextreme-vue/number-box';
 
 <template>
     <!-- <input type="number"> -->
-    <div class="x-input-number">
-        <DxNumberBox :value="-2314.12" format="###,###,###.###,##"
-            :input-attr="{ 'aria-label': 'Accounting Format' }" />
+    <div>
+        <DxNumberBox class="x-input-number" :value="-2314.12" format="###,###,###.###,##" />
     </div>
 </template>
 
 <style scoped>
 .x-input-number {
     padding: 2px 5px;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         font-size: 14px;
         line-height: 20px;
         color: #24292e;
@@ -29,6 +29,17 @@ import DxNumberBox from 'devextreme-vue/number-box';
         border-radius: 2px;
         outline: none;
         box-shadow: rgba(225, 228, 232, 0.2) 0px 1px 0px 0px inset;
+        height: 26px;
 }
 
+.x-input-number:hover,
+.x-input-number:focus-visible {
+    border-color: #0366d6;
+    box-shadow: 0 0 0 1px #0366d6;
+    outline: none;
+}
+
+.x-input-number:disabled {
+    background-color: #f1f1f1;
+}
 </style>
