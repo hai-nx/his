304d4199e99207e044712fa9cdc9059210ef5005
hai-nx <72272795+hai-nx@users.noreply.github.com>
2024-09-18 01:14:06 +0700

    update

--- src/components/paginator/index.vue
+++ src/components/paginator/index.vue
@@ -1,4 +1,13 @@
 <script setup lang="ts">
+import { defineModel, defineProps } from 'vue'
+
+const current = defineModel('current')
+const pageSize = defineModel('pageSize')
+
+const props = defineProps({
+    total: { type: Number },
+    pageSizeOptions: { type: Object }
+})
 
 </script>
 
